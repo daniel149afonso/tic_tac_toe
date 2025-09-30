dc10bf09715b8eaf4b21e61ebaad880cfc8e1355
daniel <daniel-congo2@hotmail.com>
2025-09-30 19:19:11 +0200

    trying to update the name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,17 +3,15 @@ import { useState } from "react";
 export default function Player({name, symbol}) {
 	const [isEditting, setEditting] = useState(false);
 	function handleClickEdit(){
-		setEditting(true);
+		setEditting((isEditting)=>!isEditting);
 	}
 	let playerName = <span class="player-name">{name}</span>;
 	let edit = "Edit";
 	if (isEditting)
 	{
 		edit = "Save";
-		playerName = <input type="text" required/>;
+		playerName = <input type="text" required defaultValue={name}/>;
 	}
-	else
-		edit = "Edit";
 	return (
 		<li>
 			<div class="player">
