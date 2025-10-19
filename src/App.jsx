43f14974eb7576ce458297f2138151db586813fa
daniel <daniel-congo2@hotmail.com>
2025-10-19 15:34:10 +0200

    added gameBoard components to display X

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
-	const [player1, setPlayer1] = useState("Player 1");
-	const [player2, setPlayer2] = useState("Player 2");
+	const [activePlayer, setactivePlayer] = useState("X");
+
 	return (
 		<main>
 			<div id="game-container">
 				<ol id="players">
-					<Player  initialName={"Player 1"} symbol="X"/>
-					<Player  initialName={"Player 2"} symbol="O"/>
+					<Player initialName={"Player 1"} symbol="X"/>
+					<Player initialName={"Player 2"} symbol="O"/>
 				</ol>
 				<GameBoard/>
 			</div>
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -5,26 +5,34 @@ const initialGameBoard = [
 	[null, null, null],
 	[null, null, null],
 ];
-
+//Example copy array shallow copy
+const fruits = ["banana", "apple"];
+const fav = [...fruits];
+fruits[0] = "orange";
+console.log("Fruit:", fruits[0]);
+//END Example
 export default function GameBoard(){
 	const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
 	function handleClick(rowIndex, colIndex){
 		setGameBoard((prevGameBoard) => {
-			let newGameBoard = {...prevGameBoard.map(innerArray => [...innerArray])};
+			const newGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
 			newGameBoard[rowIndex][colIndex] = "X";
 			return (newGameBoard);
 		});
 	}
-	return (<ol id="game-board">
-		{initialGameBoard.map((row, rowIndex)=><li key={rowIndex}>
+	return (
+	
+	<ol id="game-board">
+		{gameBoard.map((row, rowIndex) => (
+		<li key={rowIndex}>
 			<ol>
 				{row.map((playerSymbol, colIndex)=>(
 					<li key={colIndex}>
 						<button onClick={() => handleClick(rowIndex, colIndex)}>{playerSymbol}</button>
 					</li>
-			))}
+				))}
 			</ol>
-		</li>)}
+		</li>))}
 	</ol>);
 }
