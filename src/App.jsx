0c8e6f25751f0edce779b245853a75555bc6f718
daniel <daniel-congo2@hotmail.com>
2025-10-29 21:07:07 +0100

    added thurn player handler

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,40 @@ import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
+
+	// State that stores all the turns that have been played so far
+	const [gameTurns, setGameTurns] = useState([]);
+	
+	// State that keeps track of which player's turn it currently is: "X" or "O"
 	const [activePlayer, setActivePlayer] = useState("X");
 
-	function handleActivePlayer(){
-		setActivePlayer((symbol) => symbol === "X" ? "O" : "X");
+	// Function that switches the active player after a move is made
+	function handleActivePlayer(rowIndex, colIndex){
+		setActivePlayer(symbol => symbol === "X" ? "O" : "X");
+		
+		setGameTurns(prevTurns => {
+			let currentPlayer = "X";
+
+			if (prevTurns.length > 0 && prevTurns[0].player === "X")
+				currentPlayer = "O";
+			const updatedTurns = [
+				{square: {row: rowIndex, col: colIndex}, player: currentPlayer},
+				...prevTurns,
+			];
+			return (updatedTurns);
+		});
 	}
 	return (
 		<main>
 			<div id="game-container">
+				
 				<ol id="players" className="highlight-player">
-					<Player initialName={"Player 1"} symbol="X" activeSymbol={activePlayer === "X"}/>
+					<Player initialName={"Player 1"} symbol="X" activeSymbol={activePlayer === "X"}/>{/*return a boolean true or false is active or not*/}
 					<Player initialName={"Player 2"} symbol="O" activeSymbol={activePlayer === "O"}/>
 				</ol>
 				<GameBoard onTriggerActivePlayer={handleActivePlayer} activeSymbol={activePlayer}/>
 			</div>
+			<Log/>
 			<p>{activePlayer}</p>
 		</main>
 	);
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,31 +1,31 @@
-import {useState} from "react";
-
 const initialGameBoard = [
 	[null, null, null],
 	[null, null, null],
 	[null, null, null],
 ];
 
-export default function GameBoard({onTriggerActivePlayer, activeSymbol}){
-	const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({onTriggerActivePlayer}){
+	// const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-	function handleClick(rowIndex, colIndex){
-		setGameBoard((prevGameBoard) => {
-			const newGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-			newGameBoard[rowIndex][colIndex] = activeSymbol;
-			return (newGameBoard);
-		});
-		onTriggerActivePlayer();
-	}
+	// function handleClick(rowIndex, colIndex){
+	// 	setGameBoard((prevGameBoard) => {
+	// 		const newGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+	// 		newGameBoard[rowIndex][colIndex] = activeSymbol;
+	// 		return (newGameBoard);
+	// 	});
+	// 	onTriggerActivePlayer();
+	// }
+
+
 	return (
-	
+
 	<ol id="game-board">
 		{gameBoard.map((row, rowIndex) => (
 		<li key={rowIndex}>
 			<ol>
 				{row.map((playerSymbol, colIndex)=>(
 					<li key={colIndex}>
-						<button onClick={() => handleClick(rowIndex, colIndex)}>{playerSymbol}</button>
+						<button onClick={onTriggerActivePlayer}>{playerSymbol}</button>
 					</li>
 				))}
 			</ol>
