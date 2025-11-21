484b1beec820425ba125d06f84f6079101551335
daniel <daniel-congo2@hotmail.com>
2025-11-21 12:05:39 +0100

    Log finished

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -3,8 +3,10 @@ export default function Log({turns}){
 	return <ol id="log">
 		{turns.map((turn, colIndex)=>(
 			<li key={`${turn.square.row}${turn.square.col}`}>
-				Player: {turn.player}
-				Index: {turn.square.row}, {turn.square.col}
+				<p>
+					Player: {turn.player + ", "}
+					Index: {turn.square.row}, {turn.square.col}
+				</p>
 			</li>
 		))}
 	</ol>
