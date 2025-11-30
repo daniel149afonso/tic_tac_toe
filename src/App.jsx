e2d396ba270224a7c095d1bc4cb56cbd86d1e2c4
daniel <daniel-congo2@hotmail.com>
2025-11-30 14:35:21 +0100

    added win list items

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
+import { WINNING_COMBINATIONS } from "./winning_list_items.js";
 
 function App() {
 
