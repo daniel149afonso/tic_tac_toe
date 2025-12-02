7d0117ca96fc812fc756977edc3bad37690ff232
daniel <daniel-congo2@hotmail.com>
2025-12-02 22:09:21 +0100

    added winning combinations

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
 import { WINNING_COMBINATIONS } from "./winning_combinations.js";
 
+const initialGameBoard = [
+	[null, null, null],
+	[null, null, null],
+	[null, null, null],
+];
+
 function deriveActivePlayer(gameTurns){
 	let currentPlayer = "X";
 	if (gameTurns.length > 0 && gameTurns[0].player === "X")
@@ -12,11 +18,29 @@ function deriveActivePlayer(gameTurns){
 }
 
 function App() {
-
 	// State that stores all the turns that have been played so far
 	const [gameTurns, setGameTurns] = useState([]);
 	const activePlayer = deriveActivePlayer(gameTurns);
-	
+
+	let gameBoard = initialGameBoard;
+	//Fill the array with the player historic
+	for (const turn of gameTurns) {
+		// turn = un coup dans la liste
+		const {square, player} = turn;
+		const {row, col} = square;
+		gameBoard[row][col] = player;
+	}
+
+	let winner = null;
+	for (const combination of WINNING_COMBINATIONS) {
+		const firstSquareSymbol = gameBoard[combination[0].row][combination[0].col];
+		const secondSquareSymbol = gameBoard[combination[1].row][combination[1].col];
+		const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].col];
+
+		if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol)
+			winner = firstSquareSymbol;
+	}
+
 	// Function that switches the active player after a move is made
 	function handleActivePlayer(rowIndex, colIndex) {
 		// Save the played turn and who played X or O
@@ -36,7 +60,8 @@ function App() {
 					<Player initialName={"Player 1"} symbol="X" activeSymbol={gameTurns.player === "X"}/>{/*return a boolean true or false is active or not*/}
 					<Player initialName={"Player 2"} symbol="O" activeSymbol={gameTurns.player === "O"}/>
 				</ol>
-				<GameBoard onTriggerActivePlayer={handleActivePlayer} turns={gameTurns}/>
+				{winner && <p>You win {winner}!</p>}
+				<GameBoard onTriggerActivePlayer={handleActivePlayer} board={gameBoard}/>
 			</div>
 			<Log turns={gameTurns}/>
 		</main>
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,24 +1,7 @@
-const initialGameBoard = [
-	[null, null, null],
-	[null, null, null],
-	[null, null, null],
-];
-
-export default function GameBoard({onTriggerActivePlayer, turns}){
-	let gameBoard = initialGameBoard;
-	//Remplit le tableau avec l'historique des joueurs
-	for (const turn of turns) {
-	// turn = un coup dans la liste
-	const square = turn.square; // ex: {row:0, col:1}
-	const player = turn.player; // ex: "X"
-	const row = square.row; // 0
-	const col = square.col; // 1
-	gameBoard[row][col] = player;
-}
-
+export default function GameBoard({onTriggerActivePlayer, board}){
 	return (
 	<ol id="game-board">
-		{gameBoard.map((row, rowIndex) => (
+		{board.map((row, rowIndex) => (
 		<li key={rowIndex}>
 			<ol>
 				{row.map((playerSymbol, colIndex)=>(
